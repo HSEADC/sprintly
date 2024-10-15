3ae54bdf50bf073ec685a7d005f882e277c7d7dc
grinberg-kris <kgrinberg17@gmail.com>
2024-10-15 17:10:16 +0500

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,168 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'docs'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env']]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(ttf|otf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    }),
+
+    // Index
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+
+    // Изучение AGILE
+    new HtmlWebpackPlugin({
+      template: './src/about-agile.html',
+      filename: './about-agile.html'
+    }),
+
+    // Cтатьи
+    new HtmlWebpackPlugin({
+      template: './src/articles/articles.html',
+      filename: './articles/articles.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/articles/article_1.html',
+      filename: './articles/article_1.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/articles/article_2.html',
+      filename: './articles/article_2.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/articles/article_3.html',
+      filename: './articles/article_3.html'
+    }),
+
+    // Платформа
+    new HtmlWebpackPlugin({
+      template: './src/platform.html',
+      filename: './platform.html'
+    }),
+
+    // Веб-плакат
+    new HtmlWebpackPlugin({
+      template: './src/motivation.html',
+      filename: './motivation.html'
+    }),
+
+    // Книги
+    new HtmlWebpackPlugin({
+      template: './src/books/books.html',
+      filename: './books/books.html'
+    })
+
+    // Article
+    // new HtmlWebpackPlugin({
+    //   template: './src/articles/superorganisms/S_Popup.html',
+    //   filename: './superorganisms/S_Popup.html'
+    // }),
+
+    // Partials
+    // new HtmlWebpackPartialsPlugin([
+    //   {
+    //     path: path.join(__dirname, './src/partials/analytics.html'),
+    //     location: 'analytics',
+    //     template_filename: '*',
+    //     priority: 'replace'
+    //   }
+    // ])
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
+  }
+}
